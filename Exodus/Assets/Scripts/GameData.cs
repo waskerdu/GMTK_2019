20b23e73e13b40b9475dce6a5fe19bdfda8850e5
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-07-29 13:37:38 -0700

    Added basic save/load and utilities

---
+++ Exodus/Assets/Scripts/GameData.cs
@@ -0,0 +1,26 @@
+﻿using Malee;
+using System;
+using static Utilities;
+
+/* When upgrading post-launch, tag new fields with:
+ * [OptionalField(VersionAdded = int)]
+ * This allows old save files to be upgraded to the new format
+ * Remember to initialize any fields that might import as null before saving again.
+*/
+
+#region GameData
+[Serializable]
+public struct GameData
+{
+    public PlayerInfo playerInfo;
+}
+#endregion
+
+#region Player Info
+[Serializable]
+public struct PlayerInfo
+{
+    public Vector3Serializer position;
+    public Vector2Serializer rotation;
+}
+#endregion
