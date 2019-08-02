dac810f6d3d081d8d29fe161cbc31dd0261f73e7
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-08-02 14:50:46 -0700

    Camera Manager with zoom added

---
+++ Exodus/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraManager : MonoBehaviour
+{
+    public Vector3 outZoomPosition;
+    public Vector3 inZoomPosition;
+    public float outZoomSize;
+    public float inZoomSize;
+
+    Camera cam;
+    //0 = zoomed in, 1 = zoomed out
+    float zoomPos = 0;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        SetZoom();
+    }
+
+    private void Update()
+    {
+        float newZoom = -Input.GetAxisRaw("Vertical");
+
+        if (newZoom != 0)
+        {
+            zoomPos = Mathf.Clamp01(zoomPos + newZoom * Time.deltaTime);
+            SetZoom();
+        }
+    }
+
+    public void SetZoom()
+    {
+        transform.position = Vector3.Lerp(inZoomPosition, outZoomPosition, zoomPos);
+        cam.orthographicSize = Mathf.Lerp(inZoomSize, outZoomSize, zoomPos);
+    }
+}
