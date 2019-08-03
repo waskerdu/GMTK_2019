d0a50258cd02af83eb69934fd814e0018ab23ea5
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2019-08-03 15:20:48 -0700

    removed some debug logs, add mech sprites

--- Exodus/Assets/Scripts/SwarmCall.cs
+++ Exodus/Assets/Scripts/SwarmCall.cs
@@ -6,24 +6,20 @@ public class SwarmCall : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Swarm Call Triggered");
         if (!gameObject.activeInHierarchy) return;
 
         if (collision.GetComponent<Enemy>())
         {
-            Debug.Log("FoundEnemy");
             collision.SendMessage("Swarm", GetComponentInParent<Enemy>().gameObject);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Swarm Call Triggered");
         if (!gameObject.activeInHierarchy) return;
 
         if (collision.gameObject.GetComponent<Enemy>())
         {
-            Debug.Log("FoundEnemy");
             collision.gameObject.SendMessage("Swarm", GetComponentInParent<Enemy>().gameObject);
         }
     }
